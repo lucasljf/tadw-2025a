978b85bb2da0a3abbfd319891f81c320ecefd63a
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2025-03-12 10:55:27 -0300

    Update listarClientes.php

--- codigo/listarClientes.php
+++ codigo/listarClientes.php
@@ -1,44 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Lista de clientes</h1>
 
-    <table border="1">
-        <tr>
-            <td>Id</td>
-            <td>Nome</td>
-            <td>CPF</td>
-            <td>Endereço</td>
-            <td colspan="2">Ação</td>
-        </tr>
-            <?php
-                require_once "conexao.php";
+    <?php
+    require_once "conexao.php";
+    require_once "funcoes.php";
 
-                $sql = "SELECT * FROM tb_cliente";
+    $lista_clientes = listarClientes($conexao);
+    
+    //verificar se encontrou clientes antes de imprimir.
+    if (count($lista_clientes) == 0) {
+        echo "Não existem clientes cadastrados.";
+    } else {
+    ?>
+        <table border="1">
+            <tr>
+                <td>Id</td>
+                <td>Nome</td>
+                <td>CPF</td>
+                <td>Endereço</td>
+                <td colspan="2">Ação</td>
+            </tr>
 
-                $resultados = mysqli_query($conexao, $sql);
+        <?php
+        foreach ($lista_clientes as $cliente) {
+            $idcliente = $cliente['idcliente'];
+            $nome = $cliente['nome'];
+            $cpf = $cliente['cpf'];
+            $endereco = $cliente['endereco'];
 
-                while ($linha = mysqli_fetch_array($resultados)) {
-                    $id = $linha['idcliente'];
-                    $nome = $linha['nome'];
-                    $cpf = $linha['cpf'];
-                    $endereco = $linha['endereco'];
-
-                    echo "<tr>";
-                    echo "<td>$id</td>";
-                    echo "<td>$nome</td>";
-                    echo "<td>$cpf</td>";
-                    echo "<td>$endereco</td>";
-                    echo "<td><a href='deletarCliente.php?id=$id'>Excluir</a></td>";
-                    echo "<td><a href='formCliente.php?id=$id'>Editar</a></td>";
-                    echo "</tr>";
-                }
-            ?>
-    </table>
+            echo "<tr>";
+            echo "<td>$idcliente</td>";
+            echo "<td>$nome</td>";
+            echo "<td>$cpf</td>";
+            echo "<td>$endereco</td>";
+            echo "<td><a href='formCliente.php?id=$idcliente'>Editar</a></td>";
+            echo "<td><a href='deletarCliente.php?id=$idcliente'>Excluir</a></td>";
+            echo "</tr>";
+        }
+    }
+        ?>
+        </table>
 </body>
+
 </html>
