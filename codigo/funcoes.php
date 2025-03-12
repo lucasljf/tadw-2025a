20d75098f73d083868c76c6bf3a057447e718f9b
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2025-03-12 10:53:06 -0300

    Criação de funções e seus testes

---
+++ codigo/funcoes.php
@@ -0,0 +1,48 @@
+<?php
+
+function deletarCliente($conexao, $idcliente) {
+    $sql = "DELETE FROM tb_cliente WHERE idcliente = ?";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_bind_param($comando, 'i', $idcliente);
+    $funcionou = mysqli_stmt_execute($comando);
+
+    mysqli_stmt_close($comando);
+    
+    return $funcionou; //true ou false
+}
+
+function listarClientes($conexao) {
+    $sql = "SELECT * FROM tb_cliente";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_execute($comando);
+    $resultado = mysqli_stmt_get_result($comando);
+
+    $lista_clientes = [];
+    while ($cliente = mysqli_fetch_assoc($resultado)) {
+        $lista_clientes[] = $cliente;
+    }
+
+    mysqli_stmt_close($comando);
+    return $lista_clientes;
+}
+
+function salvarCliente($conexao, $nome, $cpf, $endereco) {
+    $sql = "INSERT INTO tb_cliente (nome, cpf, endereco) VALUES (?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'sss', $nome, $cpf, $endereco);
+    
+    $funcionou = mysqli_stmt_execute($comando);
+    
+    mysqli_stmt_close($comando);
+    return $funcionou;
+};
+
+function deletarProduto($conexao, $idproduto) {
+    
+};
+
+function listarProdutos() {};
+?>
