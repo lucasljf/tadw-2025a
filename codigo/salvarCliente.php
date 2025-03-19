7f59600ee4891fbc9cdfe36aaaf43d9dff7aa720
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2025-03-19 10:04:51 -0300

    Update salvarCliente.php

--- codigo/salvarCliente.php
+++ codigo/salvarCliente.php
@@ -1,5 +1,6 @@
 <?php
 require_once "conexao.php";
+require_once "funcoes.php";
 
 $id = $_GET['id'];
 $nome = $_POST['nome'];
@@ -7,11 +8,7 @@ $cpf = $_POST['cpf'];
 $endereco = $_POST['endereco'];
 
 if ($id == 0) {
-    $sql = "INSERT INTO tb_cliente (nome, cpf, endereco) VALUES (?, ?, ?)";
-
-    $comando = mysqli_prepare($conexao, $sql);
-
-    mysqli_stmt_bind_param($comando, 'sss', $nome, $cpf, $endereco);
+    salvarCliente($conexao, $nome, $cpf, $endereco);
 } else {
     $sql = "UPDATE tb_cliente SET nome=?, cpf=?, endereco=? WHERE idcliente=?";
 
@@ -19,6 +16,3 @@ if ($id == 0) {
 
     mysqli_stmt_bind_param($comando, 'sssi', $nome, $cpf, $endereco, $id);
 }
-mysqli_stmt_execute($comando);
-
-mysqli_stmt_close($comando);
