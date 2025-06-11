73fdc6cf6b4ab6e93cc5df93d710ae40bcff7dbf
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2025-06-11 08:39:32 -0300

    Salvar foto cliente
    Alteração do formulário e criação do processo de carregamento da foto.

--- codigo/salvarCliente.php
+++ codigo/salvarCliente.php
@@ -7,8 +7,25 @@ $nome = $_POST['nome'];
 $cpf = $_POST['cpf'];
 $endereco = $_POST['endereco'];
 
+$nome_arquivo = $_FILES['foto']['name'];
+$caminho_temporario = $_FILES['foto']['tmp_name'];
+
+// pega a extensão do arquivo
+$extensao = pathinfo($nome_arquivo, PATHINFO_EXTENSION);
+
+//gera um novo nome para o arquivo
+$novo_nome = uniqid() . "." . $extensao;
+
+//criando um novo caminho para o arquivo (usando o endereço da página)
+//lembre-se de criar a pasta "fotos/" dentro da pasta "codigo".
+//deve ajustar as permissões da pasta "fotos".
+$caminho_destino = "fotos/" . $novo_nome;
+
+//movendo o arquivo para o servidor
+move_uploaded_file($caminho_temporario, $caminho_destino);
+
 if ($id == 0) {
-    salvarCliente($conexao, $nome, $cpf, $endereco);
+    salvarCliente($conexao, $nome, $cpf, $endereco, $novo_nome);
 } else {
     editarCliente($conexao, $nome, $cpf, $endereco, $id);
 }
